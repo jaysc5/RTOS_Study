22fcf0a5d92e8bae4328477733094fe4390fa9de
jaysc5 <jaysc5@ajou.ac.kr>
2023-10-12 23:23:05 +0900

    [OS chapter 11.1, 11.2] Event flag

--- boot/Main.c
+++ boot/Main.c
@@ -50,6 +50,7 @@ static void Kernel_init(void)
     uint32_t taskId;
 
     Kernel_task_init();
+    Kernel_event_flag_init();
 
     taskId = Kernel_task_create(User_task0);
     if (NOT_ENOUGH_TASK_NUM == taskId)
@@ -101,9 +102,17 @@ void User_task0(void)
 {
     uint32_t local = 0;
 
+    debug_printf("User Task #0 SP=0x%x\n", &local);
+
     while(true)
     {
-    	debug_printf("User Task #0 SP=0x%x\n", &local);
+        KernelEventFlag_t handle_event = Kernel_wait_events(KernelEventFlag_UartIn);
+        switch(handle_event)
+        {
+        case KernelEventFlag_UartIn:
+            debug_printf("\nEvent handled\n");
+            break;
+        }
         Kernel_yield();
     }
 }
@@ -111,10 +120,10 @@ void User_task0(void)
 void User_task1(void)
 {
     uint32_t local = 0;
-
+    
+    debug_printf("User Task #1 SP=0x%x\n", &local);
     while(true)
     {
-        debug_printf("User Task #1 SP=0x%x\n", &local);
         Kernel_yield();
     }
 }
@@ -122,10 +131,10 @@ void User_task1(void)
 void User_task2(void)
 {
     uint32_t local = 0;
-
+    
+    debug_printf("User Task #2 SP=0x%x\n", &local);
     while(true)
     {
-        debug_printf("User Task #2 SP=0x%x\n", &local);
         Kernel_yield();
     }
 }
--- hal/rvpb/Uart.c
+++ hal/rvpb/Uart.c
@@ -1,7 +1,9 @@
 #include "stdint.h"
+#include "stdbool.h"
 #include "Uart.h"
 #include "HalUart.h"
 #include "HalInterrupt.h"
+#include "Kernel.h"
 
 extern volatile PL011_t* Uart;
 
@@ -53,4 +55,6 @@ static void interrupt_handler(void)
 {
     uint8_t ch = Hal_uart_get_char();
     Hal_uart_put_char(ch);
+    
+    Kernel_send_events(KernelEventFlag_UartIn);
 }
